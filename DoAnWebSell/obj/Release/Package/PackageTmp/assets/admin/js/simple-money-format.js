48c371e898992c35534f93d0ac689d38e3ddd1c0
nnson58th2 <nhuson2306@gmail.com>
2019-06-29 08:49:36 +0700

    Include hidden items and publish project

---
+++ DoAnWebSell/obj/Release/Package/PackageTmp/assets/admin/js/simple-money-format.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+    let money = $('.price').val();
+    let price = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'VND' }).format(money);
+    console.log(price);
+});
